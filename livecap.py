9ae3573ec5da3e3dadabe05081052b3cd514c311
KW-Kylie <61099190+KW-Kylie@users.noreply.github.com>
2020-03-27 10:42:53 -0400

    Update livecap.py

--- livecap.py
+++ livecap.py
@@ -7,12 +7,6 @@ cap = pyshark.LiveCapture(interface='eth0', bpf_filter='udp port 53')
 
 cap.sniff(packet_count=5)
 
-def print_dns_info(pkt):
-	if pkt.dns.qry_name:
-		print("DNS Request from %s: %s" % (pkt.ip.src, pkt.dns.qry_name))
-	elif pkt.dns.resp_name:
-		print("DNS Request from %s: %s" % (pkt.ip.src, pkt.dns.resp_name))
-
 for pkt in cap:
 	sys.stdout = open(filename, 'w')
 	print(pkt)
